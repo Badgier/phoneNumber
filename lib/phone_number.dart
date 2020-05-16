0e589b23be3036823aa48485a744ba9de2d00b61
Badger <lukasz.borkowski2525@gmail.com>
2020-05-16 17:49:17 +0200

    now it's definitely working

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,18 +1,77 @@
 class PhoneNumber {
-  String clean(String number){
+  String clean(String number) {
     String phoneNumber = number;
-
-    if (phoneNumber.startsWith('+1')) {
-      phoneNumber = phoneNumber.substring(2);  
-    } else if (phoneNumber.startsWith('1')){
-      phoneNumber = phoneNumber.substring(1);
-    }
-
     phoneNumber = phoneNumber.replaceAll('.', '');
     phoneNumber = phoneNumber.replaceAll(' ', '');
     phoneNumber = phoneNumber.replaceAll('(', '');
     phoneNumber = phoneNumber.replaceAll(')', '');
     phoneNumber = phoneNumber.replaceAll('-', '');
-    return phoneNumber;
+    phoneNumber = phoneNumber.replaceAll('+', '');
+
+    if (phoneNumber.contains(
+      RegExp(r'[a-zA-Z]'),
+    )) {
+      throw FormatException("letters not permitted");
+    }
+    if (number.contains(
+      RegExp(r'[@:!]'),
+    )) {
+      throw FormatException("punctuations not permitted");
+    }
+
+    if (phoneNumber.length != 10) {
+      if (phoneNumber.length < 10) {
+        throw FormatException("incorrect number of digits");
+      }
+      if (phoneNumber.length > 10) {
+        if (phoneNumber.length == 11) {
+          if (phoneNumber.startsWith('1')) {
+            phoneNumber = phoneNumber.substring(1);
+            if (phoneNumber.startsWith('0')) {
+              throw FormatException("area code cannot start with zero");
+            }
+            if (phoneNumber.startsWith('1')) {
+              throw FormatException("area code cannot start with one");
+            }
+            if (phoneNumber[3] == '0') {
+              throw FormatException("exchange code cannot start with zero");
+            }
+            if (phoneNumber[3] == '1') {
+              throw FormatException("exchange code cannot start with one");
+            } else {
+              return phoneNumber;
+            }
+          } else {
+            throw FormatException("11 digits must start with 1");
+          }
+        }
+        if (phoneNumber.length > 11) {
+          throw FormatException("more than 11 digits");
+        } else {
+          if (phoneNumber.startsWith('+1')) {
+            phoneNumber = phoneNumber.substring(2);
+          }
+        }
+      }
+    }
+    if (phoneNumber.startsWith('0') ||
+        phoneNumber.startsWith('1') ||
+        phoneNumber[3] == '0' ||
+        phoneNumber[3] == '1') {
+      if (phoneNumber.startsWith('0')) {
+        throw FormatException("area code cannot start with zero");
+      }
+      if (phoneNumber.startsWith('1')) {
+        throw FormatException("area code cannot start with one");
+      }
+      if (phoneNumber[3] == '0') {
+        throw FormatException("exchange code cannot start with zero");
+      } else {
+        throw FormatException("exchange code cannot start with one");
+      }
+    } else {
+      print(phoneNumber);
+      return phoneNumber;
+    }
   }
 }
